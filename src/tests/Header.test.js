0818ab6ec3946d15f8ed5bfd479f428ba2f81156
Wael Elsafty <waelelsafty07@gmail.com>
2023-06-23 13:05:28 +0300

    Fix: eslint

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../app/store';
 import Header from '../Components/Header';
+
 describe('Header test', () => {
   it('renders the Header', () => {
     const headerTest = renderer
